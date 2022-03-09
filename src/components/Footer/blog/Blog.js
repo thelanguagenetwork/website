941a9d0c34cd5b26740a7209f1cc7aa9ebfab77b
mainaksaha07 <mainaksaha@ternaengg.ac.in>
2022-03-09 11:21:17 +0530

    blog

---
+++ src/components/Footer/blog/Blog.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MediumSlider from './MediumSlider'
+
+const Blog = () => {
+
+  return (
+    <div>
+      <div className="container">
+        <MediumSlider/>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
