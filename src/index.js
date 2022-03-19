e720410e4b7915c168cdea1b6175ff0a7f4ffe54
Pratik733 <pratik.tayade733@gmail.com>
2022-03-19 11:55:40 +0530

    dictionary update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import "swiper/css/bundle";
 import 'tw-elements';
+import 'flowbite';
 
 
 ReactDOM.render(
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",'./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'
+    "./node_modules/flowbite/**/*.js","./src/**/*.{js,jsx,ts,tsx}",'./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('tw-elements/dist/plugin')],
+  plugins: [require('tw-elements/dist/plugin'),require('flowbite/plugin')],
 }
+// 'tw-elements/dist/plugin',
