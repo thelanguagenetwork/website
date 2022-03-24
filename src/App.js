fb36e0517a8a60eaa7413174058e4a3cdf644537
Pratik733 <pratik.tayade733@gmail.com>
2022-03-24 20:21:57 +0530

    footer pages adding

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import Curriculum from './components/curriculum/Curriculum';
 import AdultsCourses from './components/CoursePricing/AdultCourses';
 import IntroCourse from './components/Footer/LangForAdults/IntroCourse';
 import Dict from './components/Footer/dict/dict';
-import Blog from './components/Footer/blog/Blog';
+import Blog from './components/Footer/Company/blog/Blog';
 import AdultsEnglish from './components/Footer/LangForAdults/English';
 import AdultsFrench from './components/Footer/LangForAdults/French';
 import AdultsSpanish from './components/Footer/LangForAdults/Spanish';
@@ -24,6 +23,8 @@ import KidsGerman from './components/Footer/LangForKids/German';
 import KidsMandarin from './components/Footer/LangForKids/Mandarin';
 import KidsJapanese from './components/Footer/LangForKids/Japanese';
 import KidsKorean from './components/Footer/LangForKids/Korean';
+import AboutUs from './components/Footer/Company/AboutUs/AboutUs.js';
+import Careers from './components/Footer/Company/Careers/Careers';
 
 
 function App() {
@@ -55,6 +56,8 @@ function App() {
           <Route exact path='/korean-course-kids' element={<KidsKorean />} />
           <Route exact path='/Dictionary' element={<Dict />} />
           <Route exact path='/Blog' element={<Blog/>} />
+          <Route exact path='/AboutUs' element={<AboutUs />} />
+          <Route exact path='/Careers' element={<Careers />} />
         </Routes>
         <Footer />
       </Router>
