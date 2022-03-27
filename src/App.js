19583f4d64f56140d5b7790fa593c0684f260551
Pratik733 <pratik.tayade733@gmail.com>
2022-03-27 12:29:45 +0530

    loader and more

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import React, { Suspense } from 'react'
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import AdultsCourses from './components/CoursePricing/AdultCourses';
-import IntroCourse from './components/Footer/LangForAdults/IntroCourse';
-import Dict from './components/Footer/dict/dict';
-import Blog from './components/Footer/Company/blog/Blog';
-import AdultsEnglish from './components/Footer/LangForAdults/English';
-import AdultsFrench from './components/Footer/LangForAdults/French';
-import AdultsSpanish from './components/Footer/LangForAdults/Spanish';
-import AdultsGerman from './components/Footer/LangForAdults/German';
-import AdultsMandarin from './components/Footer/LangForAdults/Mandarin';
-import AdultsJapanese from './components/Footer/LangForAdults/Japanese';
-import AdultsKorean from './components/Footer/LangForAdults/Korean';
 
-import KidsEnglish from './components/Footer/LangForKids/English';
-import KidsFrench from './components/Footer/LangForKids/French';
-import KidsSpanish from './components/Footer/LangForKids/Spanish';
-import KidsGerman from './components/Footer/LangForKids/German';
-import KidsMandarin from './components/Footer/LangForKids/Mandarin';
-import KidsJapanese from './components/Footer/LangForKids/Japanese';
-import KidsKorean from './components/Footer/LangForKids/Korean';
-import AboutUs from './components/Footer/Company/AboutUs/AboutUs.js';
-import Careers from './components/Footer/Company/Careers/Careers';
 import Loader from './components/Loader';
 
 
--- src/Routing.js
+++ src/Routing.js
@@ -25,7 +25,9 @@ import KidsJapanese from './components/Footer/LangForKids/Japanese';
 import KidsKorean from './components/Footer/LangForKids/Korean';
 import AboutUs from './components/Footer/Company/AboutUs/AboutUs.js';
 import Careers from './components/Footer/Company/Careers/Careers';
-import Loader from './components/Loader';
+import Aff_prog from './components/Footer/Subscribe/aff_prog';
+import Faqs from './components/Footer/Subscribe/faqs';
+import Contact from './components/Footer/Subscribe/Contact';
 
 
 function Routing() {
@@ -59,6 +61,13 @@ function Routing() {
           <Route exact path='/Blog' element={<Blog/>} />
           <Route exact path='/AboutUs' element={<AboutUs />} />
           <Route exact path='/Careers' element={<Careers />} />
+
+          <Route exact path='/Faqs' element={<Faqs />} />
+
+          <Route exact path='/Contact' element={<Contact />} />
+
+          <Route exact path='/Aff_prog' element={<Aff_prog />} />
+
         </Routes>
         <Footer />
       </Router>
