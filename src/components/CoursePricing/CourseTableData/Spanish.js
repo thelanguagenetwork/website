197ad6878dc73fc83f0b9fbece9eb11093453d49
mainaksaha07 <mainaksaha@ternaengg.ac.in>
2022-04-10 19:39:44 +0530

    spanish price

--- src/components/CoursePricing/CourseTableData/Spanish.js
+++ src/components/CoursePricing/CourseTableData/Spanish.js
@@ -3,51 +3,51 @@ export const Spanish_1_1 = [
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A1',
       //  OgPrice : 444,
-        DiscPrice : 333,
+        DiscPrice : 27990,
         Link: "https://rzp.io/l/spanisha111"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A2',
       //  OgPrice : 444,
-        DiscPrice : 333,
+        DiscPrice : 45990,
         Link: "https://rzp.io/l/spanisha21"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'B1',
       //  OgPrice : 444,
-        DiscPrice : 333,
+        DiscPrice : 59980,
         Link: "https://rzp.io/l/spanishb11"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'B2',
-        OgPrice : 444,
-      //  DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 61980,
         Link: "https://rzp.io/l/spanishb21"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A1+A2',
-        OgPrice : 444,
-       // DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 59190,
         Link: "https://rzp.io/l/spanisha1a21"
     },
     {
 
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'B1+B2',
-        OgPrice : 444,
-       // DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 97590,
         Link: "https://rzp.io/l/spnaishb1b21"
     },
     {
 
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A1+A2+B1+B2',
-        OgPrice : 444,
-       // DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 156790,
         Link: "https://rzp.io/l/spanisha1a2b1b21"
     }
 ]
@@ -56,50 +56,50 @@ export const Spanish_1_2 = [
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A1',
-        OgPrice : 444,
-       // DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 16790,
         Link: "https://rzp.io/l/QInsHGRu"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A2',
-        OgPrice : 444,
-      //  DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 27990,
         Link: "https://rzp.io/l/PRisneS"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'B1.1 + B1.2',
-        OgPrice : 444,
-     //   DiscPrice : 333,
+     //   OgPrice : 444,
+        DiscPrice : 45580,
         Link: "https://rzp.io/l/cmFaBrL"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'B2.1 + B2.2',
-        OgPrice : 444,
-     //   DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 47980,
         Link: "https://rzp.io/l/spanishb22"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A1+A2',
-        OgPrice : 444,
-      //  DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 35890,
         Link: "https://rzp.io/l/spnaisha1a22"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'B1+B2',
-        OgPrice : 444,
-       // DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 74890,
         Link: "https://rzp.io/l/spanishb1b22"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A1+A2+B1+B2',
-        OgPrice : 444,
-      //  DiscPrice : 333,
+       // OgPrice : 444,
+       DiscPrice : 110690,
         Link: "https://rzp.io/l/aw5RJe7T"
     }
 ]
@@ -109,50 +109,50 @@ export const Spanish_1_4 = [
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A1',
-        OgPrice : 444,
-       // DiscPrice : 333,
+       // OgPrice : 444,
+       DiscPrice : 11990,
         Link: "https://rzp.io/l/Spanisha1"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A2',
-        OgPrice : 444,
-       // DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 19190,
         Link: "https://rzp.io/l/spanisha2"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'B1',
-        OgPrice : 444,
-      //  DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 35980,
         Link: "https://rzp.io/l/spanishb1"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'B2',
-        OgPrice : 444,
-//DiscPrice : 333,
+       // OgPrice : 444,
+       DiscPrice : 38380,
         Link: "https://rzp.io/l/Spanishb2"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A1+A2',
-        OgPrice : 444,
-       // DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 25590,
         Link: "https://rzp.io/l/Spanisha1a2"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'B1+B2',
-        OgPrice : 444,
-      //  DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 59490,
         Link: "https://rzp.io/l/spanishb1b2"
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'A1+A2+B1+B2',
-        OgPrice : 444,
-       // DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice :84990 ,
         Link: "https://rzp.io/l/spanisha1a2b1b2"
     }
 ]
