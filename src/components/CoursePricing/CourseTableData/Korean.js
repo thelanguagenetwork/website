f7bdc7d106fe0b3017620f42749264b8a83fadfb
mainaksaha07 <mainaksaha@ternaengg.ac.in>
2022-04-16 14:57:14 +0530

    korean price

--- src/components/CoursePricing/CourseTableData/Korean.js
+++ src/components/CoursePricing/CourseTableData/Korean.js
@@ -2,52 +2,52 @@ export const Korean_1_1 = [
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1 Level 1',
-        OgPrice : 444,
-        DiscPrice : 333,
+//OgPrice : 444,
+        DiscPrice : 21990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1 Level 2',
-        OgPrice : 444,
-        DiscPrice : 333,
+        //OgPrice : 444,
+        DiscPrice : 25990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 2 Level 3',
-        OgPrice : 444,
-        DiscPrice : 333,
+        //OgPrice : 444,
+        DiscPrice : 29990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 2 Level 4',
-        OgPrice : 444,
-        DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 29990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1 Level 1+2',
-        OgPrice : 444,
-        DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 38390,
         Link: ""
     },
     {
 
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 2 Level 3+4',
-        OgPrice : 444,
-        DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 47990,
         Link: ""
     },
     {
 
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1+2',
-        OgPrice : 444,
-        DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 80990,
         Link: ""
     }
 ]
@@ -56,50 +56,50 @@ export const Korean_1_2 = [
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1 Level 1',
-        OgPrice : 444,
-        DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 15990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1 Level 2',
-        OgPrice : 444,
-        DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice :19990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 2 Level 3',
-        OgPrice : 444,
-        DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 24990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 2 Level 4',
-        OgPrice : 444,
-        DiscPrice : 333,
+     //   OgPrice : 444,
+        DiscPrice : 24990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1 Level 1+2',
-        OgPrice : 444,
-        DiscPrice : 333,
+       //OgPrice : 444,
+        DiscPrice :28790,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 2 Level 3+4',
-        OgPrice : 444,
-        DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice :39990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1+2',
-        OgPrice : 444,
-        DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 64990,
         Link: ""
     },
 ]
@@ -109,50 +109,50 @@ export const Korean_1_4 = [
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1 Level 1',
-        OgPrice : 444,
-        DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice :9990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1 Level 2',
-        OgPrice : 444,
-        DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 13990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 2 Level 3',
-        OgPrice : 444,
-        DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 19990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 2 Level 4',
-        OgPrice : 444,
-        DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 19990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1 Level 1+2',
-        OgPrice : 444,
-        DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 19990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 2 Level 3+4',
-        OgPrice : 444,
-        DiscPrice : 333,
+       // OgPrice : 444,
+        DiscPrice : 31990,
         Link: ""
     },
     {
         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
         lvlName: 'Topik 1+2 ',
-        OgPrice : 444,
-        DiscPrice : 333,
+      //  OgPrice : 444,
+        DiscPrice : 47990,
         Link: ""
     },
 ]
