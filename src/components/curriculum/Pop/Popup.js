20ed583624bf486b4ac9dd60ef93eb20b6210c73
HanselDsouza <hanseld54@gmail.com>
2022-05-07 21:18:33 +0530

    popup redifined

--- src/components/curriculum/Pop/Popup.js
+++ src/components/curriculum/Pop/Popup.js
@@ -3,22 +3,28 @@ import { Button, Modal } from 'react-bootstrap';
 import "./Popup.css"
 function MyVerticallyCenteredModal(props) {
     return (
-
-        <Modal className="flex items-center justify-center rounder-xl"
+        <Modal className=" flex items-center justify-center"
             {...props}
             size="lg"
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
-            <Modal.Header className="flex justify-center mx-0" closeButton>
-                <Modal.Title  id="contained-modal-title-vcenter flex align-center items-center justify-center">
-                    <span>Select you age group</span>
-                    
+            {/* <button onClick={props.onHide} type="button" class="close btn-val" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span>
+            </button> */}
+            <div className="flow-root px-10 items-end">
+            <button onClick={props.onHide} className="btn-val border-0 focus:outline-none  pt-3 float-right w-1/8 font-bold text-black bg-[#fffff]  hover:bg-white hover:text-black border-white"><span className="text-2xl" aria-hidden="true">×</span>
+            </button>
+            </div>
+            <Modal.Header className="flex mx-0 " closeButton>
+                <Modal.Title id="contained-modal-title-vcenter  w-full ">
+                    <span className="text-lg lg:text-2xl text-left w-full px-10">Select you age group</span>
                 </Modal.Title>
             </Modal.Header>
+
             <Modal.Body>
                 <div className="lg:flex flex-col items-center">
-                    <div className="flex flex-row divide-x">
+                    <div className="flex flex-row divide-x pb-10 lg:pb-16">
 
                         <div className="flex justify-center w-1/2 justify-center items-center">
                             <div className="flex flex-col content-center justify-center items-center  ">
@@ -40,9 +46,6 @@ function MyVerticallyCenteredModal(props) {
                     </div>
                 </div>
             </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide} className="font-bold text-black bg-[#13bfab] border-1 hover:bg-white border-black  hover:text-black">Close</Button>
-            </Modal.Footer>
         </Modal>
 
     );
@@ -54,7 +57,7 @@ function Popup(pop) {
     return (
         <>  <div className="space-y-2">
             <span onClick={() => setModalShow(true)} class="border-transparent border-0 btn-val hover:cursor-pointer" >
-                
+
                 <div className='p-6 md:w-1/2 lg:w-3/4 flex flex-col items-center'>
                     <img src={pop.im} alt="img" className='w-[9rem] lg:w-[19rem]' />
                     <h3 className='font-medium text-2xl mt-3 text-black text-center'>{pop.sub}</h3>
