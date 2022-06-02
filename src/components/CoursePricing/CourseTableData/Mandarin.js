bfa9007c52a3ebb89d9cef315cfaa9fe53f85e39
mainaksaha07 <mainaksaha@ternaengg.ac.in>
2022-06-02 18:42:57 +0530

    mandarin

--- src/components/CoursePricing/CourseTableData/Mandarin.js
+++ src/components/CoursePricing/CourseTableData/Mandarin.js
@@ -32,14 +32,17 @@ export const Mandarin_1_1 = [
         DiscPrice : 17990,
         Link: ""
     },
-    // {
-    //     language: "Mandarin",
-    //     content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
-    //     lvlName: 'HSK 4',
-    //   //  OgPrice : 444,
-    //     DiscPrice : 23990,
-    //     Link: ""
-    // },
+     {
+        title: 'Professional Knowledge',
+        includes:'Degree of fluency, Express Opinions, Complex Grammar Structures.',
+        
+       language: "Mandarin",
+         content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
+         lvlName: 'HSK 4',
+     // OgPrice : 444,
+         DiscPrice : 23990,
+         Link: ""
+     },
     // {
     //     language: "Mandarin",
     //     content: ['Study material','Mock test','Certified courses ','International exam preparation','Instant feedback'],
