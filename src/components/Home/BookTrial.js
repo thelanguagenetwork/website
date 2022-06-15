56cce295c70aa728a47ea8f7afb414753fc44ca1
mainaksaha07 <mainaksaha@ternaengg.ac.in>
2022-06-15 18:47:38 +0530

    commit

--- src/components/Home/BookTrial.js
+++ src/components/Home/BookTrial.js
@@ -14,7 +14,8 @@ else {
 const { definition, styles, l10n, locale, translations, snapshot, attachments, onSubmit, onPause } = Services.init({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiejZqSjhMTFRONlJ4ZW95MmRBNFNjcnkzQytCaWpES2tTbTdNSnFmRUpKQT0iLCJkZWZpbml0aW9uIjoiWHUvZ0c5SkZ1aGNMY0xIR1A1TGQ2WU10VzY4cUZUcW03cUVOOWZ6Qy93dz0iLCJ0eXBlIjoiY29sbGVjdCJ9.n2GA2CLltkyfdUrJHtMpxdrTIf8-VzXwHoiDbSlCTKA" });
 
 run({
-    element: document.body, 
+    element: doc, /* Or supply your own element here */
+    definition,
     styles,
     l10n,
     locale,
