0298196fd4a5f721a4fdff9f7198ed20f5379800
mainaksaha07 <mainaksaha@ternaengg.ac.in>
2022-06-26 11:19:06 +0530

    commit

--- src/components/curriculum/Pop/Popupmain.js
+++ src/components/curriculum/Pop/Popupmain.js
@@ -64,7 +64,7 @@ function MyVerticallyCenteredModal(props) {
                             </a>
                         </div>
                         <div className='self-center'>
-                            <a href="https://api.whatsapp.com/send/?phone=%2B919821392630&text&app_absent=0">
+                            <a href="https://api.whatsapp.com/send/?phone=%2B919137239887&text&app_absent=0">
                                 <button className='gs'>Get in touch with us</button>
 
                             </a>
